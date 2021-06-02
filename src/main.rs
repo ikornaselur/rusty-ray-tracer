556ee223ae12d48ade0a5c6d5da59f3fc2a10a2c
Axel Örn <dev@absalon.is>
2021-06-02 16:27:34 +0100

    Simplifying the Ray-Sphere intersection code

--- src/main.rs
+++ src/main.rs
@@ -77,15 +77,15 @@ fn main() -> std::io::Result<()> {
 
 fn hit_sphere(center: Point3, radius: f32, ray: Ray) -> f32 {
     let oc = ray.origin - center;
-    let a = ray.direction.dot(ray.direction);
-    let b = 2.0 * oc.dot(ray.direction);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = ray.direction.length_squared();
+    let half_b = oc.dot(ray.direction);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
 
     return if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / a
     };
 }
 
